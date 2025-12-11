ea4e70cdd2f1e7726bd7899a0cd09076e67c1255
LLVSGMV <llvs@gmv.com>
2025-12-11 17:52:22 +0100

    Delete intall Docker in init.sh

--- Scripts/init.sh
+++ Scripts/init.sh
@@ -69,25 +69,6 @@ else
     echo -e "\nYou said that you don't have a proxy.\n\n"
 fi
 
-###############################################
-# Install Docker + Docker Compose v2
-###############################################
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
-  sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
-  https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo $UBUNTU_CODENAME) stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update -y
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-sudo systemctl enable --now docker
-sudo usermod -aG docker ubuntu
-
 #################################################
 #####                Main               #########
 #################################################
